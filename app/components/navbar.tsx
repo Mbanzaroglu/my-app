9e7a296e28b29b4c363ab24586f1183cc5bf37b3
Mbanzaroglu <banzaroglu20@itu.edu.tr>
2025-02-04 10:37:39 +0300

    Router is removed

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 import Link from "next/link"
-import { usePathname, useRouter } from "next/navigation"
+import { usePathname} from "next/navigation"
 import { Menu, X } from "lucide-react"
 import { motion, AnimatePresence } from "framer-motion"
 import { useLocale } from "@/contexts/LocaleContext"
